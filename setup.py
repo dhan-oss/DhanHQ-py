1de75d94679985200f4bf08762aee9106d9da9f0
Dhan <112463199+dhan-oss@users.noreply.github.com>
2024-10-01 12:19:43 +0530

    Update setup.py version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 HERE = pathlib.Path(__file__).parent
 
-VERSION = '1.3.2'
+VERSION = '1.3.3'
 PACKAGE_NAME = 'dhanhq'
 AUTHOR = 'Dhan'
 AUTHOR_EMAIL = 'dhan-oss@dhan.co'
