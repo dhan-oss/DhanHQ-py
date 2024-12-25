c6c164695039f07cdc8a80eac2da37f652e69431
karthiks <karthiks@users.noreply.github.com>
2024-12-25 13:07:30 +0530

    Configured project with flake8 to check py code against PEP8 stds

--- init-dev-env.sh
+++ init-dev-env.sh
@@ -3,4 +3,5 @@ source .venv/bin/activate
 pip list
 alias pyt='pytest --cache-clear -s'
 alias pyl='pylint . --output-format=colorized "$FilePath$"'
+alias flk='flake8'
 echo "You are now good for development"
