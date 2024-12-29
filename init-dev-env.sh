5652b10483866d4c3965d746c931490a3fb64e1f
karthiks <karthiks@users.noreply.github.com>
2024-12-29 12:40:42 +0530

    Ignoring tests dir from linting by pylint and flake8

--- init-dev-env.sh
+++ init-dev-env.sh
@@ -4,6 +4,6 @@ pip list
 alias pytu='pytest tests/unit --cache-clear -s'
 alias pyti='pytest tests/integration --cache-clear -s'
 alias pyt='pytest --cache-clear -s'
-alias pyl='pylint . --output-format=colorized "$FilePath$"'
+alias pyl='pylint . --output-format=colorized'
 alias flk='flake8'
 echo "You are now good for development"
